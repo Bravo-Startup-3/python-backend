e28e0d5c61506505eb5c854bf833629bdb2169ae
Agadaga Joseph <josephagadaga@gmail.com>
2022-11-01 20:44:09 +0100

    Register users to admin

--- users/admin.py
+++ users/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import User, Influencer, Brand, Admin
 
-# Register your models here.
+
+admin.site.register(User)
+admin.site.register(Brand)
+admin.site.register(Influencer)
+admin.site.register(Admin)
