09e526c4c4d5847bbf7be50e30da93d19fd1d6c2
Agadaga Joseph <josephagadaga@gmail.com>
2022-11-01 20:39:04 +0100

    Blog update

--- blog/forms.py
+++ blog/forms.py
@@ -21,24 +21,8 @@ class PostForm(forms.ModelForm):
 class CommentForm(forms.Form):
     content_type = forms.CharField(widget=forms.HiddenInput)
     object_id = forms.IntegerField(widget=forms.HiddenInput)
-    #parent_id = forms.IntegerField(widget=forms.HiddenInput, required=False)
+    parent_id = forms.IntegerField(widget=forms.HiddenInput, required=False)
     content = forms.CharField(label='', widget=forms.Textarea)
 
 
 
-'''from django import forms
-
-class CommentForm(forms.Form):
-    author = forms.CharField(
-        max_length=60,
-        widget=forms.TextInput(attrs={
-            "class": "form-control",
-            "placeholder": "Your Name"
-        })
-    )
-    body = forms.CharField(widget=forms.Textarea(
-        attrs={
-            "class": "form-control",
-            "placeholder": "Leave a comment!"
-        })
-    )'''
--- blog/models.py
+++ blog/models.py
@@ -183,33 +183,3 @@ class Comment(models.Model):
             return False
         return True
 
-
-'''class Category(models.Model):
-    name = models.CharField(max_length=60)
-
-    def __str__(self):
-        return self.name
-
-
-class Post(models.Model):
-    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='author')
-    title = models.CharField(max_length=255, null=True, blank=True)
-    sub_title = models.CharField(max_length=255, null=True, blank=True)
-    image = models.ImageField(upload_to='static/blog', null=True, blank=True)
-    body = RichTextUploadingField(config_name='blog_ckeditor', null=True, blank=True)
-    created_on = models.DateTimeField(auto_now_add=True)
-    last_modified = models.DateTimeField(auto_now=True)
-    categories = models.ManyToManyField('Category', related_name='posts')
-    slug = models.SlugField(max_length=250, unique_for_date='publish')
-
-    def __str__(self):
-        return self.title
-
-class Comment(models.Model):
-    author = models.CharField(max_length=60)
-    body = models.TextField()
-    created_on = models.DateTimeField(auto_now_add=True)
-    post = models.ForeignKey('Post', on_delete=models.CASCADE)
-
-    def __str__(self):
-        return self.author'''
